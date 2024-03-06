dce77a68c2a99b293ae220e7954b797537011165
okonu <ianyakundi015@gmail.com>
2024-03-06 15:52:28 +0300

    add enable filament auth features, change color to blue

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -27,8 +27,12 @@ class AppPanelProvider extends PanelProvider
             ->id('app')
             ->path('')
             ->login()
+            // ->registration()
+            ->passwordReset()
+            ->emailVerification()
+            // ->profile()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
