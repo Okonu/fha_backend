873ebfba3bd7f6399045b6ac0285d082471c5448
okonu <ianyakundi015@gmail.com>
2024-03-11 09:34:55 +0300

    cors allowed origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://www.foundershubafrica.com', 'https://www.foundershub.africa'],
 
     'allowed_origins_patterns' => [],
 
@@ -30,5 +30,6 @@ return [
     'max_age' => 0,
 
     'supports_credentials' => false,
+
 
 ];
